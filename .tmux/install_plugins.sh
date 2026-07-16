95788217656625cfa4a26d697566a74448a25ec0
GreenShyDude <PhantomTentacle@proton.me>
2026-07-16 23:38:27 +0200

    removed unused stuff tmux

---
+++ .tmux/install_plugins.sh
@@ -0,0 +1,5 @@
+rm -rf plugins
+mkdir plugins
+git clone https://github.com/tmux-plugins/tpm plugins
+mkdir plugins/catppuccin
+git clone -b v2.3.0 https://github.com/catppuccin/tmux.git plugins/catppuccin
